c0ccbcabcd2ab5ab11febddfeb30d76e4a1b433a
Bernard Wagner <s10156225@gmail.com>
2017-07-27 07:02:56 +0200

    --amend

---
+++ objection/hooks/android/hooking/list-broadcast-receivers.js
@@ -0,0 +1,32 @@
+// Lists the loaded classes that extend BroadcastReceiver available in the current Java
+// runtime.
+
+var BroadcastReceiver = Java.use("android.content.BroadcastReceiver")
+
+var classes = Java.enumerateLoadedClassesSync()
+
+
+var receivers = classes.filter(function(className){
+    //Exclude some classes to prevent Java.use blocking (Some memory management issue)
+    if (className.startsWith("android") || className.startsWith("java") || className.startsWith("com.android") || !className.includes(".")) return false;
+
+    //Some classes are not in search path resulting in Java.use throwing exception
+    try{
+        var clazz = Java.use(className);
+        var isReceiver = BroadcastReceiver.class.isAssignableFrom(clazz.class);
+        clazz.$dispose();
+        return isReceiver;
+    } catch (e){}
+    return false;
+})
+
+
+var response = {
+    status: 'success',
+    error_reason: NaN,
+    type: 'android-broadcast-receivers',
+    data: receivers
+}
+
+send(JSON.stringify(response));
+
