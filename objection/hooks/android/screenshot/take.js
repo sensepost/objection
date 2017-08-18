70314b5d8909931f471f626fbc41c2996a930763
Leon Jacobs <leonja511@gmail.com>
2017-08-18 18:53:00 +0200

    Remove unused use() statements and formatting fixes.

--- objection/hooks/android/screenshot/take.js
+++ objection/hooks/android/screenshot/take.js
@@ -1,17 +1,17 @@
-var WindowManager = Java.use("android.view.WindowManager");
-var ActivityThread = Java.use('android.app.ActivityThread');
-var Activity = Java.use("android.app.Activity");
-var ActivityClientRecord = Java.use("android.app.ActivityThread$ActivityClientRecord");
-var ArrayMap = Java.use('android.util.ArrayMap');
-var Bitmap = Java.use("android.graphics.Bitmap");
-var File = Java.use("java.io.File");
-var ByteArrayOutputStream = Java.use("java.io.ByteArrayOutputStream");
-var CompressFormat = Java.use("android.graphics.Bitmap$CompressFormat");
-var Base64 = Java.use('android.util.Base64');
+// Take a screenshot by making use of a View's drawing cache:
+//  ref: https://developer.android.com/reference/android/view/View.html#getDrawingCache(boolean)
 
-var data;
+var ActivityThread = Java.use('android.app.ActivityThread');
+var Activity = Java.use('android.app.Activity');
+var ActivityClientRecord = Java.use('android.app.ActivityThread$ActivityClientRecord');
+var Bitmap = Java.use('android.graphics.Bitmap');
+var ByteArrayOutputStream = Java.use('java.io.ByteArrayOutputStream');
+var CompressFormat = Java.use('android.graphics.Bitmap$CompressFormat');
+
+var bytes;
 
 var populate_bytes = function () {
+
     var activityThread = ActivityThread.currentActivityThread();
     var currentApplication = ActivityThread.currentApplication();
     var context = currentApplication.getApplicationContext();
@@ -20,27 +20,34 @@ var populate_bytes = function () {
 
     var currentActivity;
 
-    for (var i in activityRecords){
-        var activityRecord =  Java.cast(activityRecords[i],ActivityClientRecord);
-        if (!activityRecord.paused['value']){
-            currentActivity = Java.cast(Java.cast(activityRecord,ActivityClientRecord).activity['value'],Activity);
+    for (var i in activityRecords) {
+
+        var activityRecord = Java.cast(activityRecords[i], ActivityClientRecord);
+
+        if (!activityRecord.paused['value']) {
+            currentActivity = Java.cast(Java.cast(activityRecord, ActivityClientRecord)
+                .activity['value'], Activity);
+
             break;
         }
     }
 
-    if (currentActivity){
+    if (currentActivity) {
+
         var view = currentActivity.getWindow().getDecorView().getRootView();
         view.setDrawingCacheEnabled(true);
         var bitmap = Bitmap.createBitmap(view.getDrawingCache());
         view.setDrawingCacheEnabled(false);
+
         var outputStream = ByteArrayOutputStream.$new();
-        bitmap.compress(CompressFormat.PNG['value'],100,outputStream); 
+        bitmap.compress(CompressFormat.PNG['value'], 100, outputStream);
         bytes = outputStream.buf['value'];
-    } 
+    }
 }
 
 rpc.exports = {
     screenshot: function () {
+
         Java.perform(function () { populate_bytes(); });
         return bytes;
     },
