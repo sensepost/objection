1882eb98adebd0f6265848e9d69d7eb4dd5a2abc
Leon Jacobs <leonja511@gmail.com>
2017-09-06 17:16:59 +0200

    Add `android keystore list` command files.

---
+++ objection/commands/android/keystore.py
@@ -0,0 +1,32 @@
+import click
+from tabulate import tabulate
+
+from objection.utils.frida_transport import FridaRunner
+from objection.utils.templates import android_hook
+
+
+def entries(args: list = None) -> None:
+    """
+        Lists entries in the Android KeyStore
+
+        :param args:
+        :return:
+    """
+
+    runner = FridaRunner()
+    runner.set_hook_with_data(android_hook('keystore/list'))
+    runner.run()
+
+    response = runner.get_last_message()
+
+    if not response.is_successful():
+        click.secho('Failed to list keystore items with error: {0}'.format(response.error_reason), fg='red')
+        return None
+
+    if not response.data:
+        click.secho('No keystore items were found', fg='yellow')
+        return None
+
+    output = [[x['alias'], x['is_key'], x['is_certificate']] for x in response.data]
+
+    click.secho(tabulate(output, headers=['Alias', 'Is Key', 'Is Certifiate']))
---
+++ objection/hooks/android/keystore/list.js
@@ -0,0 +1,47 @@
+// Dump entries in the Android Keystore, together with a flag
+// indicating if its a key or a certificate.
+//
+// Ref: https://developer.android.com/reference/java/security/KeyStore.html
+
+var KeyStore = Java.use('java.security.KeyStore');
+var entries = [];
+
+// Prepare the AndroidKeyStore keystore provider and load it. 
+// Maybe at a later stage we should support adding other stores
+// like from file or JKS.
+var ks = KeyStore.getInstance('AndroidKeyStore');
+ks.load(null, null);
+
+// Get the aliases and loop through them. The aliases() method
+// return an Enumeration<String> type.
+var aliases = ks.aliases();
+
+while (aliases.hasMoreElements()) {
+
+    var alias = aliases.nextElement();
+
+    entries.push({
+        'alias': alias.toString(),
+        'is_key': ks.isKeyEntry(alias),
+        'is_certificate': ks.isCertificateEntry(alias)
+    })
+}
+
+var response = {
+    status: 'success',
+    error_reason: NaN,
+    type: 'android-keystore',
+    data: entries
+}
+
+send(JSON.stringify(response));
+
+// - Sample Java
+//
+// KeyStore ks = KeyStore.getInstance("AndroidKeyStore");
+// ks.load(null);
+// Enumeration<String> aliases = ks.aliases();
+//
+// while(aliases.hasMoreElements()) {
+//     Log.e("E", "Aliases = " + aliases.nextElement());
+// }
