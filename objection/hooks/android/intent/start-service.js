f031a229c324734e04315daabf3cd4e4571a76f3
BlackAp3rture <twhitmi@gmail.com>
2017-09-21 14:12:14 -0400

    Update start-service.js

--- objection/hooks/android/intent/start-service.js
+++ objection/hooks/android/intent/start-service.js
@@ -12,12 +12,11 @@ var new_service = Java.use('{{ intent_class }}').class;
 
 // Init and launch the intent
 var intent = Intent.$new(context, new_service);
-intent.setFlags(0x10000000);    // Intent.FLAG_ACTIVITY_NEW_TASK
+intent.setFlags(0x10000000);   
 context.startService(intent);
 
 // -- Sample Java
 //
-// Intent intent = new Intent(this, DisplayMessageActivity.class);
-// intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+// Intent intent = new Intent(this, Service.class);
 //
-// startActivity(intent);
+// startService(intent);
