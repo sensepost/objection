5b19247890eb54b7fd8af83c7d3c83b6f392e1c9
Julien Legras <julien.legras@synacktiv.com>
2017-10-17 10:18:36 +0200

    Added arguments and return value print in android hooking method with --dump-args

--- objection/hooks/android/hooking/watch-method.js
+++ objection/hooks/android/hooking/watch-method.js
@@ -5,7 +5,7 @@
 var Throwable = Java.use('java.lang.Throwable');
 
 var target_class = Java.use('{{ target_class }}');
-var overload_count = eval('target_class.{{ target_method }}.overloads.length');
+var overload_count = target_class.{{ target_method }}.overloads.length;
 
 send({
     status: 'success',
@@ -25,19 +25,20 @@ for (var i = 0; i < overload_count; i++) {
     });
 
     // Hook the overload.
-    eval('target_class.{{ target_method }}.overloads[i]').implementation = function () {
+    target_class.{{ target_method }}.overloads[i].implementation = function () {
 
         var message = 'Called {{ target_class }}.{{ target_method }} (args: ' + arguments.length + ')';
 
         // if we should include a backtrace to here, do that.
-        if ('{{ include_backtrace }}' == 'True') {
+        {% if include_backtrace %}
 
             message += '\nBacktrace:\n\t' + Throwable.$new().getStackTrace()
                 .map(function (stack_trace_element) {
 
                     return stack_trace_element.toString() + '\n\t';
                 }).join('');
-        }
+
+        {% endif %}
 
         send({
             status: 'success',
@@ -46,7 +47,32 @@ for (var i = 0; i < overload_count; i++) {
             data: message
         });
 
+        {% if dump_args %}
+            // Loop the arguments and dump the values.toString()
+            for (var h = 0; h < arguments.length; h++) {
+
+                send({
+                    status: 'success',
+                    error_reason: NaN,
+                    type: 'watch-class-method',
+                    data: '{{ target_method }} - Arg ' + h + ': ' + arguments[h].toString()
+                });
+            }
+        {% endif %}
+
+
         // continue with the original method
-        return eval('this.{{ target_method }}.apply(this, arguments)');
+        var ret = this.{{ target_method }}.apply(this, arguments);
+        {% if dump_args %}
+            send({
+                status: 'success',
+                error_reason: NaN,
+                type: 'watch-class-method',
+                data: '{{ target_method }} - Returned : ' + ret.toString()
+            });
+        {% endif %}
+
+        return ret;
+
     }
 }
