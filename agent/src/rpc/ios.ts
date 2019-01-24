07cb69f200db8474cf3d4e664383cdf95a799224
Leon Jacobs <leonja511@gmail.com>
2019-01-24 15:56:21 +0200

    Define return types for the iOS RPC.

--- agent/src/rpc/ios.ts
+++ agent/src/rpc/ios.ts
@@ -4,8 +4,8 @@ import { iosfilesystem } from "../ios/filesystem";
 import { hooking } from "../ios/hooking";
 import { iosjailbreak } from "../ios/jailbreak";
 import { ioskeychain } from "../ios/keychain";
-import { IKeychainItem } from "../ios/lib/interfaces";
-import { NSMutableDictionary, NSUserDefaults } from "../ios/lib/types";
+import { ICredential, IIosCookie, IIosFileSystem, IKeychainItem } from "../ios/lib/interfaces";
+import { NSUserDefaults } from "../ios/lib/types";
 import { nsuserdefaults } from "../ios/nsuserdefaults";
 import { pasteboard } from "../ios/pasteboard";
 import { sslpinning } from "../ios/pinning";
@@ -14,30 +14,30 @@ import { userinterface } from "../ios/userinterface";
 
 export const ios = {
   // ios binary cookies
-  iosCookiesGet: () => binarycookies.get(),
+  iosCookiesGet: (): IIosCookie[] => binarycookies.get(),
 
   // ios nsurlcredentialstorage
-  iosCredentialStorage: () => credentialstorage.dump(),
+  iosCredentialStorage: (): ICredential[] => credentialstorage.dump(),
 
   // ios filesystem
-  iosFileCwd: () => iosfilesystem.pwd(),
-  iosFileDownload: (path: string) => iosfilesystem.readFile(path),
-  iosFileExists: (path: string) => iosfilesystem.exists(path),
-  iosFileLs: (path: string) => iosfilesystem.ls(path),
-  iosFilePathIsFile: (path: string) => iosfilesystem.pathIsFile(path),
-  iosFileReadable: (path: string) => iosfilesystem.readable(path),
-  iosFileUpload: (path: string, data: string) => iosfilesystem.writeFile(path, data),
-  iosFileWritable: (path: string) => iosfilesystem.writable(path),
+  iosFileCwd: (): string => iosfilesystem.pwd(),
+  iosFileDownload: (path: string): Buffer => iosfilesystem.readFile(path),
+  iosFileExists: (path: string): boolean => iosfilesystem.exists(path),
+  iosFileLs: (path: string): IIosFileSystem => iosfilesystem.ls(path),
+  iosFilePathIsFile: (path: string): boolean => iosfilesystem.pathIsFile(path),
+  iosFileReadable: (path: string): boolean => iosfilesystem.readable(path),
+  iosFileUpload: (path: string, data: string): void => iosfilesystem.writeFile(path, data),
+  iosFileWritable: (path: string): boolean => iosfilesystem.writable(path),
 
   // ios hooking
-  iosHookingGetClassMethods: (className: string, includeParents: boolean) =>
+  iosHookingGetClassMethods: (className: string, includeParents: boolean): string[] =>
     hooking.getClassMethods(className, includeParents),
   iosHookingGetClasses: () => hooking.getClasses(),
-  iosHookingSearchMethods: (partial: string) => hooking.searchMethods(partial),
-  iosHookingSetReturnValue: (selector: string, returnVal: boolean) =>
+  iosHookingSearchMethods: (partial: string): string[] => hooking.searchMethods(partial),
+  iosHookingSetReturnValue: (selector: string, returnVal: boolean): void =>
     hooking.setMethodReturn(selector, returnVal),
-  iosHookingWatchClass: (clazz: string, parents: boolean) => hooking.watchClass(clazz, parents),
-  iosHookingWatchMethod: (selector: string, dargs: boolean, dbt: boolean, dret: boolean) =>
+  iosHookingWatchClass: (clazz: string, parents: boolean): void => hooking.watchClass(clazz, parents),
+  iosHookingWatchMethod: (selector: string, dargs: boolean, dbt: boolean, dret: boolean): void =>
     hooking.watchMethod(selector, dargs, dbt, dret),
 
   // jailbreak detection
@@ -48,16 +48,16 @@ export const ios = {
   iosPlistRead: (path: string): string => plist.read(path),
 
   // ios user interface
-  iosUiAlert: (message: string) => userinterface.alert(message),
-  iosUiBiometricsBypass: () => userinterface.biometricsBypass(),
-  iosUiScreenshot: () => userinterface.screenshot(),
-  iosUiWindowDump: () => userinterface.dump(),
+  iosUiAlert: (message: string): void => userinterface.alert(message),
+  iosUiBiometricsBypass: (): void => userinterface.biometricsBypass(),
+  iosUiScreenshot: (): any => userinterface.screenshot(),
+  iosUiWindowDump: (): string => userinterface.dump(),
 
   // ios ssl pinning
-  iosPinningDisable: (quiet: boolean) => sslpinning.disable(quiet),
+  iosPinningDisable: (quiet: boolean): void => sslpinning.disable(quiet),
 
   // ios pasteboard
-  iosMonitorPasteboard: () => pasteboard.monitor(),
+  iosMonitorPasteboard: (): void => pasteboard.monitor(),
 
   // ios keychain
   keychainAdd: (key: string, data: string): boolean => ioskeychain.add(key, data),
