344de60e8c1d0447e37b7afd7e357527e46fd86e
Leon Jacobs <leonja511@gmail.com>
2019-02-20 14:24:28 +0200

    Add information about obtaining a new agent.

--- objection/utils/agent.py
+++ objection/utils/agent.py
@@ -137,8 +137,9 @@ class Agent(object):
         """
 
         if not os.path.exists(self.agent_path):
-            raise Exception('Unable to locate Objection agent sources at: {location}'.format(
-                location=self.agent_path))
+            raise Exception('Unable to locate Objection agent sources at: {location}. '
+                            'If this is a development install, check the wiki for more '
+                            'information on building the agent.'.format(location=self.agent_path))
 
         with open(self.agent_path, 'r') as f:
             agent = f.readlines()
