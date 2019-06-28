1c47460fb37ddf80f500d2dfb0aa8e4dcbf8a945
Leon Jacobs <leonja511@gmail.com>
2019-06-28 20:02:17 +0200

    Fix tests.

--- tests/commands/android/test_heap.py
+++ tests/commands/android/test_heap.py
@@ -12,10 +12,10 @@ class TestHeap(unittest.TestCase):
             output = o
 
         self.assertEqual('Usage: android heap print_instances <class> (eg: com.example.test)\n', output)
-        self.assertFalse(mock_api.return_value.android_live_get_class_instances.called)
+        self.assertFalse(mock_api.return_value.android_heap_get_live_class_instances.called)
 
     @mock.patch('objection.state.connection.state_connection.get_api')
     def test_print_live_instances_validates_command(self, mock_api):
         instances(['java.io.File'])
 
-        self.assertTrue(mock_api.return_value.android_live_get_class_instances.called)
+        self.assertTrue(mock_api.return_value.android_heap_get_live_class_instances.called)
