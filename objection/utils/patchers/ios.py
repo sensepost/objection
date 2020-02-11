3a42ab25e793c9845ff40e99423b9334e27a1d69
Fabiano1107 <fabiano.g1107@gmail.com>
2020-02-11 14:55:23 +0100

    add flag unzip-unicode during patchipa

--- objection/utils/patchers/ios.py
+++ objection/utils/patchers/ios.py
@@ -265,7 +265,13 @@ class IosPatcher(BasePlatformPatcher):
 
         # copy the orignal ipa to the temp directory.
         shutil.copyfile(ipa_source, self.temp_file)
-
+        if (unzip_unicode):
+            print("\n\nCIAOOOOOOOOOOOO\n\n")
+        # this works for IPA which contains unicode characters
+        #with zipfile.ZipFile(self.temp_file, 'r') as ipa:
+        #    for info in ipa.infolist():
+        #        info.filename = info.filename.encode('cp437').decode('utf-8')
+        #        ipa.extract(info,self.temp_directory)
         # extract the IPA this should result in a 'Payload' directory
         ipa = zipfile.ZipFile(self.temp_file, 'r')
         ipa.extractall(self.temp_directory)
