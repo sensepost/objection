d460bd960d5c0c473b6c51e59b1abe30249aabf1
StingraySA <wvermaak@mweb.co.za>
2020-04-02 05:48:36 -0700

    Added CLI Flag

--- objection/console/cli.py
+++ objection/console/cli.py
@@ -344,9 +344,11 @@ def patchipa(source: str, gadget_version: str, codesign_signature: str, provisio
 @click.option('--script-source', '-l', default=None,
               help=('A script file to use with the the "path" config type. '
                     'Specify "libfrida-gadget.script.so" as the "path" in your config.'), show_default=False)
+@click.option('--nativelibs', '-n', default=None, 
+              help=('Change the extractNativeLibs flag to true in the AndroidManifest.xml.'), show_default=False)
 def patchapk(source: str, architecture: str, gadget_version: str, pause: bool, skip_cleanup: bool,
              enable_debug: bool, skip_resources: bool, network_security_config: bool, target_class: str,
-             use_aapt2: bool, gadget_config: str, script_source: str) -> None:
+             use_aapt2: bool, gadget_config: str, script_source: str, nativelibs: bool) -> None:
     """
         Patch an APK with the frida-gadget.so.
     """
