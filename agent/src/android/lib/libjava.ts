6a2561c1413f49d363e170573c24dd34029800b8
coderlam <coderiderlin@gmail.com>
2023-06-15 15:06:35 +0800

    fix(agent)：check and wait for the currentApplication is ready

--- agent/src/android/lib/libjava.ts
+++ agent/src/android/lib/libjava.ts
@@ -3,22 +3,46 @@
 // rpc export will sniff and resolve before returning
 // the result when its ready.
 export const wrapJavaPerform = (fn: any): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    Java.perform(() => {
-      try {
-        resolve(fn());
-      } catch (e) {
-        reject(e);
-      }
+    return new Promise((resolve, reject) => {
+        // check and wait for the currentApplication is ready
+        function waitForApplicationReady(callback) {
+            if (global.appIsReady != null) {
+                callback();
+                return;
+            }
+
+            if (Java.use("android.app.ActivityThread").currentApplication() != null) {
+                callback();
+                global.appIsReady = true;
+                return;
+            }
+            //log only once
+            if (global.log_waitAppReady == null) {
+                global.log_waitAppReady = true;
+                console.log("wait for currentApplication to be ready..");
+            }
+            setTimeout(() => {
+                waitForApplicationReady(callback);
+            }, 10);
+        }
+
+        waitForApplicationReady(() => {
+            Java.perform(() => {
+                try {
+                    resolve(fn());
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
     });
-  });
 };
 
 export const getApplicationContext = (): any => {
-  const ActivityThread = Java.use("android.app.ActivityThread");
-  const currentApplication = ActivityThread.currentApplication();
+    const ActivityThread = Java.use("android.app.ActivityThread");
+    const currentApplication = ActivityThread.currentApplication();
 
-  return currentApplication.getApplicationContext();
+    return currentApplication.getApplicationContext();
 };
 
 // A helper method to access the R class for the app.
@@ -28,7 +52,7 @@ export const getApplicationContext = (): any => {
 // Using this method, the above example would be:
 //  R("content_frame", "id")
 export const R = (name: string, type: string): any => {
-  const context = getApplicationContext();
-  // https://github.com/bitpay/android-sdk/issues/14#issue-202495610
-  return context.getResources().getIdentifier(name, type, context.getPackageName());
+    const context = getApplicationContext();
+    // https://github.com/bitpay/android-sdk/issues/14#issue-202495610
+    return context.getResources().getIdentifier(name, type, context.getPackageName());
 };
