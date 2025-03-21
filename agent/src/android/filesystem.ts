sensepost/objection

commit 3f9763bfd4179ecb20db18934430e30176cc650f

(chore) Bumping of dependencies. (#681)
* (chore) Dependency bump

* Pin to major versions

* Version pinning syntax fix

agent/src/android/filesystem.ts
@@ -1,14 +1,14 @@
 import * as fs from "fs";
-import { hexStringToBytes } from "../lib/helpers";
-import { IAndroidFilesystem } from "./lib/interfaces";
+import { hexStringToBytes } from "../lib/helpers.js";
+import { IAndroidFilesystem } from "./lib/interfaces.js";
 import {
   getApplicationContext,
   wrapJavaPerform
-} from "./lib/libjava";
+} from "./lib/libjava.js";
 import {
   File,
   JavaClass
-} from "./lib/types";
+} from "./lib/types.js";
 
 export const exists = (path: string): Promise<boolean> => {
   // -- Sample Java

agent/src/generic/http.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as httpLib from "http";
 import * as url from "url";
-import { colors as c } from "../lib/color";
+import { colors as c } from "../lib/color.js";
 
 let httpServer: httpLib.Server;
 let listenPort: number;
@@ -43,17 +43,22 @@ export const start = (pwd: string, port: number = 9000): void => {
   servePath = pwd;
 
   httpServer = httpLib.createServer((req, res) => {
-    log(`${c.greenBright(req.method)} ${req.url}`);
+    if (req.method && req.url) {
+      log(`${c.greenBright(req.method)} ${req.url}`);
+    } else {
+      log(`${c.redBright('Missing URL or request method.')}`);
+      return;
+    }
+    
+    const parsedUrl =  new URL(req.url);
 
-    const parsedUrl = url.parse(req.url);
-
-    if (parsedUrl.path === "/") {
+    if (parsedUrl.pathname === "/") {
       res.end(dirListingHTML(pwd));
       return;
     }
 
     res.setHeader("Content-type", "application/octet-stream");
-    res.end(fs.readFileSync(pwd + parsedUrl.path));
+    res.end(fs.readFileSync(pwd + parsedUrl.pathname));
   });
 
   httpServer.listen(port);
@@ -70,13 +75,12 @@ export const stop = (): void => {
   httpServer.close()
     .once("close", () => {
       log(c.blackBright(`Server closed.`));
-
-      httpServer = undefined;
+      // httpServer = undefined;
     });
 };
 
 export const status = (): void => {
-  if (httpServer) {
+  if (httpServer.listening) {
     log(`Server is running on port ` +
       `${c.greenBright(listenPort.toString())} serving ${c.greenBright(servePath)}`);
     return;

agent/src/ios/binary.ts
@@ -1,7 +1,7 @@
 // tslint:disable-next-line:no-var-requires
 const macho = require("macho");
-import * as iosfilesystem from "./filesystem";
-import { IBinaryModuleDictionary } from "./lib/interfaces";
+import * as iosfilesystem from "./filesystem.js";
+import { IBinaryModuleDictionary } from "./lib/interfaces.js";
 
 
 const isEncrypted = (cmds: any[]): boolean => {

agent/src/ios/filesystem.ts
@@ -1,15 +1,15 @@
 import * as fs from "fs";
-import { hexStringToBytes } from "../lib/helpers";
-import { getNSFileManager } from "./lib/helpers";
+import { hexStringToBytes } from "../lib/helpers.js";
+import { getNSFileManager } from "./lib/helpers.js";
 import {
   IIosFilePath,
   IIosFileSystem
-} from "./lib/interfaces";
+} from "./lib/interfaces.js";
 import {
   NSDictionary,
   NSFileManager,
   NSString as NSStringType
-} from "./lib/types";
+} from "./lib/types.js";
 
 
 // a resolved nsfilemanager instance

agent/src/ios/keychain.ts
@@ -1,26 +1,26 @@
 // dumps all of the keychain items available to the current
 // application.
-import { colors as c } from "../lib/color";
-import { reverseEnumLookup } from "../lib/helpers";
+import { colors as c } from "../lib/color.js";
+import { reverseEnumLookup } from "../lib/helpers.js";
 import {
   kSec,
   NSUTF8StringEncoding
-} from "./lib/constants";
+} from "./lib/constants.js";
 import {
   bytesToHexString,
   bytesToUTF8,
   smartDataToString
-} from "./lib/helpers";
+} from "./lib/helpers.js";
 import {
   IKeychainData,
   IKeychainItem
-} from "./lib/interfaces";
-import { libObjc } from "./lib/libobjc";
+} from "./lib/interfaces.js";
+import { libObjc } from "./lib/libobjc.js";
 import {
   NSDictionary,
   NSMutableDictionary as NSMutableDictionaryType,
   NSString as NSStringType,
-} from "./lib/types";
+} from "./lib/types.js";
 
 // keychain item times to query for
 const itemClasses = [
@@ -109,8 +109,11 @@ const enumerateKeychain = (): IKeychainData[] => {
 
     return data;
   });
+  
+  const keyChainData: IKeychainData[] = [];
+  keyChainData.concat(...itemClassResults).filter((n) => n !== undefined);
 
-  return [].concat(...itemClassResults).filter((n) => n !== undefined);
+  return keyChainData;
 };
 
 // print raw entries using some Frida magic

agent/src/rpc/ios.ts
@@ -1,21 +1,21 @@
-import * as binary from "../ios/binary";
-import * as binarycookies from "../ios/binarycookies";
-import * as bundles from "../ios/bundles";
-import * as credentialstorage from "../ios/credentialstorage";
-import * as iosfilesystem from "../ios/filesystem";
-import * as heap from "../ios/heap";
-import * as hooking from "../ios/hooking";
-import * as ioscrypto from "../ios/crypto";
-import * as iosjailbreak from "../ios/jailbreak";
-import * as ioskeychain from "../ios/keychain";
-import * as nsuserdefaults from "../ios/nsuserdefaults";
-import * as pasteboard from "../ios/pasteboard";
-import * as sslpinning from "../ios/pinning";
-import * as plist from "../ios/plist";
-import * as userinterface from "../ios/userinterface";
+import * as binary from "../ios/binary.js";
+import * as binarycookies from "../ios/binarycookies.js";
+import * as bundles from "../ios/bundles.js";
+import * as credentialstorage from "../ios/credentialstorage.js";
+import * as iosfilesystem from "../ios/filesystem.js";
+import * as heap from "../ios/heap.js";
+import * as hooking from "../ios/hooking.js";
+import * as ioscrypto from "../ios/crypto.js";
+import * as iosjailbreak from "../ios/jailbreak.js";
+import * as ioskeychain from "../ios/keychain.js";
+import * as nsuserdefaults from "../ios/nsuserdefaults.js";
+import * as pasteboard from "../ios/pasteboard.js";
+import * as sslpinning from "../ios/pinning.js";
+import * as plist from "../ios/plist.js";
+import * as userinterface from "../ios/userinterface.js";
 
-import { BundleType } from "../ios/lib/constants";
-import { NSUserDefaults } from "../ios/lib/types";
+import { BundleType } from "../ios/lib/constants.js";
+import { NSUserDefaults } from "../ios/lib/types.js";
 import {
   IBinaryModuleDictionary,
   ICredential,
@@ -24,7 +24,7 @@ import {
   IIosCookie,
   IIosFileSystem,
   IKeychainItem,
-} from "../ios/lib/interfaces";
+} from "../ios/lib/interfaces.js";
 
 
 export const ios = {
@@ -100,7 +100,7 @@ export const ios = {
   iosKeychainUpdate: (account: string, service: string, newData: string): void => 
     ioskeychain.update(account, service, newData),    
   iosKeychainEmpty: (): void => ioskeychain.empty(),
-  iosKeychainList: (smartDecode): IKeychainItem[] => ioskeychain.list(smartDecode),
+  iosKeychainList: (smartDecode: boolean = false): IKeychainItem[] => ioskeychain.list(smartDecode),
   iosKeychainListRaw: (): void => ioskeychain.listRaw(),
 
   // ios nsuserdefaults

agent/src/android/clipboard.ts
@@ -1,9 +1,9 @@
-import { colors as c } from "../lib/color";
+import { colors as c } from "../lib/color.js";
 import {
   getApplicationContext,
   wrapJavaPerform
-} from "./lib/libjava";
-import { ClipboardManager } from "./lib/types";
+} from "./lib/libjava.js";
+import { ClipboardManager } from "./lib/types.js";
 
 export const monitor = (): Promise<void> => {
   // -- Sample Java

agent/src/android/general.ts
@@ -1,4 +1,4 @@
-import { wrapJavaPerform } from "./lib/libjava";
+import { wrapJavaPerform } from "./lib/libjava.js";
 
 export const deoptimize = (): Promise<void> => {
   return wrapJavaPerform(() => {

commit bf39037478209b37f78b697e0a2d98c5db8fc957

(fix) Bugfixes and minor http server feature. (#698)
* File size check for https://github.com/sensepost/objection/issues/554
* Modified device.attach to fix https://github.com/sensepost/objection/issues/694
* Fixed http server issues introduced by https://github.com/sensepost/objection/commit/3f9763bfd4179ecb20db18934430e30176cc650f (me)
* Added support for using "../" (previously this would mess with current directory display.)
* Add quotes when file/folder has space to fix https://github.com/sensepost/objection/issues/195
* Recursive directory support for http server. Previously you could not traverse directories.

agent/src/android/filesystem.ts
@@ -1,4 +1,5 @@
-import * as fs from "fs";
+import * as fs from "frida-fs";
+import { Buffer } from "buffer";
 import { hexStringToBytes } from "../lib/helpers.js";
 import { IAndroidFilesystem } from "./lib/interfaces.js";
 import {
@@ -78,7 +79,9 @@ export const pwd = (): Promise<string> => {
 };
 
 // heavy lifting is done in frida-fs here.
-export const readFile = (path: string): Buffer => {
+export const readFile = (path: string): string | Buffer => {
+  if (fs.statSync(path).size == 0)
+    return Buffer.alloc(0);
   return fs.readFileSync(path);
 };
 

agent/src/generic/http.ts
@@ -1,4 +1,4 @@
-import * as fs from "fs";
+import * as fs from "frida-fs";
 import * as httpLib from "http";
 import * as url from "url";
 import { colors as c } from "../lib/color.js";
@@ -11,19 +11,28 @@ const log = (m: string): void => {
   c.log(`[http server] ${m}`);
 };
 
-const dirListingHTML = (p: string): string => {
+const dirListingHTML = (pwd: string, path: string): string => {
   let h = `
     <html>
       <body>
-        <h2>Index Of /</h2>
+        <h2 style="margin: 0;">Index Of ${path}</h2>
         {file_listing}
       </body>
     </html>
     `;
 
   h = h.replace(`{file_listing}`, () => {
-    return fs.readdirSync(p).map((f) => {
-      return `<a href="${f}">${f}</a>`;
+    return fs.list(pwd + decodeURIComponent(path)).map((f) => {
+      if (f.name === '.') return;
+
+      // Add a slash at the end if it is a directory.
+      var fname = f.name + (f.type == 4 ? '/' : '');
+      
+      if (path !== '/') {
+        return `<a href="${path + fname}">${fname}</a>`;
+      } else if (fname !== '../') {
+        return `<a href="${fname}">${fname}</a>`;
+      }
     }).join("<br>");
   });
 
@@ -49,16 +58,40 @@ export const start = (pwd: string, port: number = 9000): void => {
       log(`${c.redBright('Missing URL or request method.')}`);
       return;
     }
-    
-    const parsedUrl =  new URL(req.url);
 
-    if (parsedUrl.pathname === "/") {
-      res.end(dirListingHTML(pwd));
-      return;
+    try {    
+      const parsedUrl = url.parse(req.url);  
+      const fileLocation = pwd + decodeURIComponent(parsedUrl.path);
+      
+      if (fs.statSync(fileLocation).isDirectory()) {
+        res.end(dirListingHTML(pwd, parsedUrl.path));
+        return;
+      }
+
+      res.setHeader("Content-type", "application/octet-stream");
+
+      // Check that we are not reading an empty file
+      if (fs.statSync(fileLocation).size !== 0) {
+        const file = fs.readFileSync(fileLocation);
+        res.write(file, 'utf-8')
+      }
+      res.end();
+        
+    } catch (error) {
+      if (error instanceof Error && error.message == "No such file or directory") {
+        res.statusCode = 404;
+        res.end("File not found")
+      } else {
+        if (error instanceof Error) {
+          log(c.redBright(`${error.stack}`));
+        } else {
+          log(c.redBright(`${error}`));
+        }
+       
+        res.statusCode = 500;
+        res.end("Internal Server Error")
+      }
     }
-
-    res.setHeader("Content-type", "application/octet-stream");
-    res.end(fs.readFileSync(pwd + parsedUrl.pathname));
   });
 
   httpServer.listen(port);
@@ -75,12 +108,12 @@ export const stop = (): void => {
   httpServer.close()
     .once("close", () => {
       log(c.blackBright(`Server closed.`));
-      // httpServer = undefined;
+      httpServer = undefined;
     });
 };
 
 export const status = (): void => {
-  if (httpServer.listening) {
+  if (httpServer && httpServer.listening) {
     log(`Server is running on port ` +
       `${c.greenBright(listenPort.toString())} serving ${c.greenBright(servePath)}`);
     return;

agent/src/ios/binary.ts
@@ -31,6 +31,9 @@ export const info = (): IBinaryModuleDictionary => {
 
     const imports: Set<string> = new Set(a.enumerateImports().map((i) => i.name));
     const fb = iosfilesystem.readFile(a.path);
+    if (typeof(fb) == 'string') {
+      return;
+    }
 
     try {
       const exe = macho.parse(fb);

agent/src/ios/filesystem.ts
@@ -1,4 +1,5 @@
-import * as fs from "fs";
+import * as fs from "frida-fs";
+import { Buffer } from "buffer";
 import { hexStringToBytes } from "../lib/helpers.js";
 import { getNSFileManager } from "./lib/helpers.js";
 import {
@@ -87,7 +88,9 @@ export const pwd = (): string => {
 };
 
 // heavy lifting is done in frida-fs here.
-export const readFile = (path: string): Buffer => {
+export const readFile = (path: string): string | Buffer => {
+  if (fs.statSync(path).size == 0)
+    return Buffer.alloc(0);
   return fs.readFileSync(path);
 };
 

agent/src/ios/keychain.ts
@@ -111,9 +111,7 @@ const enumerateKeychain = (): IKeychainData[] => {
   });
   
   const keyChainData: IKeychainData[] = [];
-  keyChainData.concat(...itemClassResults).filter((n) => n !== undefined);
-
-  return keyChainData;
+  return keyChainData.concat(...itemClassResults).filter((n) => n !== undefined);
 };
 
 // print raw entries using some Frida magic

agent/src/rpc/ios.ts
@@ -40,7 +40,7 @@ export const ios = {
   // ios filesystem
   iosFileCwd: (): string => iosfilesystem.pwd(),
   iosFileDelete: (path: string): boolean => iosfilesystem.deleteFile(path),
-  iosFileDownload: (path: string): Buffer => iosfilesystem.readFile(path),
+  iosFileDownload: (path: string): string | Buffer => iosfilesystem.readFile(path),
   iosFileExists: (path: string): boolean => iosfilesystem.exists(path),
   iosFileLs: (path: string): IIosFileSystem => iosfilesystem.ls(path),
   iosFilePathIsFile: (path: string): boolean => iosfilesystem.pathIsFile(path),

objection/commands/filemanager.py
@@ -55,7 +55,9 @@ def cd(args: list) -> None:
         return
 
     # moving one directory back
-    if path == '..':
+    device_path_separator = device_state.platform.path_separator
+
+    if path == '..' or path == '..'+device_path_separator:
 
         split_path = os.path.split(current_dir)
 
@@ -76,6 +78,10 @@ def cd(args: list) -> None:
 
         # assume the path does not exist by default
         does_exist = False
+
+        # normalise path to remove '../'
+        if '..'+device_path_separator in path:
+            path = os.path.normpath(path).replace('\\', device_path_separator)
 
         # check for existence based on the runtime
         if device_state.platform == Ios:
@@ -100,7 +106,13 @@ def cd(args: list) -> None:
     # see if its legit.
     else:
 
-        proposed_path = device_state.platform.path_separator.join([current_dir, path])
+        proposed_path = device_path_separator.join([current_dir, path])
+
+        # normalise path to remove '../'
+        if '..'+device_path_separator in proposed_path:
+            proposed_path = os.path.normpath(proposed_path).replace('\\', device_path_separator)
+            if proposed_path == '//':
+                return
 
         # assume the proposed_path does not exist by default
         does_exist = False
@@ -853,7 +865,10 @@ def list_folders_in_current_fm_directory() -> dict:
         file_name, file_type = entry
 
         if file_type == 'directory':
-            resp[file_name] = file_name
+            if ' ' in file_name:
+                resp[f"'{file_name}'"] = file_name
+            else:
+                resp[file_name] = file_name
 
     return resp
 
@@ -884,7 +899,10 @@ def list_files_in_current_fm_directory() -> dict:
         file_name, file_type = entry
 
         if file_type == 'file':
-            resp[file_name] = file_name
+            if ' ' in file_name:
+                resp[f"'{file_name}'"] = file_name
+            else:
+                resp[file_name] = file_name
 
     return resp
 

objection/utils/agent.py
@@ -216,6 +216,8 @@ class Agent(object):
 
         elif self.config.spawn:
             if self.config.uid is not None:
+                if self.device.query_system_parameters()['os']['id'] != 'android':
+                    raise Exception('--uid flag can only be used on Android.')
                 self.pid = self.device.spawn(self.config.name, uid=int(self.config.uid))
             else:
                 self.pid = self.device.spawn(self.config.name)
@@ -245,9 +247,10 @@ class Agent(object):
             raise Exception('A PID needs to be set before attach()')
 
         if self.config.uid is None:
+            debug_print(f'Attaching to PID: {self.pid}')
             self.session = self.device.attach(self.pid)
         else:
-            self.session = self.device.attach(self.pid, uid=self.config.uid)
+            self.session = self.device.attach(self.pid)
 
         self.session.on('detached', self.handlers.session_on_detached)
 